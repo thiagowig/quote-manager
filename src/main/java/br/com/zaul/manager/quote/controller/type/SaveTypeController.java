25e5f08210c71645220bf486604a20a04d71e7c8
thiago <dev.thiago@gmail.com>
2013-10-09 22:09:37 -0300

    321

--- src/main/java/br/com/zaul/manager/quote/controller/type/SaveTypeController.java
+++ src/main/java/br/com/zaul/manager/quote/controller/type/SaveTypeController.java
@@ -3,7 +3,6 @@ package br.com.zaul.manager.quote.controller.type;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import br.com.zaul.manager.quote.business.service.contract.TypeService;
@@ -14,19 +13,15 @@ public class SaveTypeController {
 
 	private Type type;
 	
-	//@EJB
-	//private TypeService service;
+	@EJB
+	private TypeService service;
 	
 	public void save() {
-		//this.service.save(this.type);
+		this.service.save(this.type);
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Tipo inserido com sucesso"));
 	}
 
 	public Type getType() {
-		if (type == null) {
-			type = new Type();
-		}
-		
 		return type;
 	}
 
