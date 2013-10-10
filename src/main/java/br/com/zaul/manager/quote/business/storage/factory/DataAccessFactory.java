8908cf0808c80ffee0a7b64199c898b5864ef378
thiago <dev.thiago@gmail.com>
2013-10-10 00:01:07 -0300

    Commit from JBoss Tools

--- src/main/java/br/com/zaul/manager/quote/business/storage/factory/DataAccessFactory.java
+++ src/main/java/br/com/zaul/manager/quote/business/storage/factory/DataAccessFactory.java
@@ -14,6 +14,12 @@ public class DataAccessFactory {
 	
 	private static final Integer MONGO_PORT = Integer.valueOf(System.getenv("OPENSHIFT_MONGODB_DB_PORT"));
 	
+	private static final String DATABASE_NAME = "quotemanager";
+	
+	private static final String ROOT_USER = "admin";
+	
+	private static final String ROOT_PASSWORD = "AD6G-6PHiHpT";
+	
 	private MongoClient mongoClient;
 	
 	private DB db;
@@ -26,7 +32,8 @@ public class DataAccessFactory {
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-		this.db = this.mongoClient.getDB("quotemanager");
+		this.db = this.mongoClient.getDB(DATABASE_NAME);
+		this.db.authenticate(ROOT_USER, ROOT_PASSWORD.toCharArray());
 		
 		return this.db;
 	}
