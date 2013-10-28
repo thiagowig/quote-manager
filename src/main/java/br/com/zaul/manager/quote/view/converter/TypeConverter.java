3267d88d095654cdb9b59d3159fa9137841cd797
thiago <dev.thiago@gmail.com>
2013-10-27 21:53:22 -0300

    321

--- src/main/java/br/com/zaul/manager/quote/view/converter/TypeConverter.java
+++ src/main/java/br/com/zaul/manager/quote/view/converter/TypeConverter.java
@@ -1,14 +1,16 @@
 package br.com.zaul.manager.quote.view.converter;
 
+import java.util.List;
+
+import javax.el.ELContext;
+import javax.el.ValueExpression;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UISelectItems;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
-import br.com.zaul.manager.quote.business.service.contract.TypeService;
 import br.com.zaul.manager.quote.business.service.entity.Type;
 
 @FacesConverter(value="typeConverter", forClass=Type.class)
@@ -16,14 +18,25 @@ public class TypeConverter implements Converter {
 
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
-		try {
-			TypeService typeService = (TypeService) new InitialContext().lookup("global/quotemanager/TypeServiceImpl");
-			Type type = typeService.findByName(value);
-			return type;
-		} catch (NamingException e) {
-			throw new ConverterException();
+		List<UIComponent> l = component.getChildren();
+		for (UIComponent uic : l) {
+			if (uic.getFamily().equals("javax.faces.SelectItems")) {
+				UISelectItems uis = (UISelectItems) uic;
+
+				ValueExpression valueExpression = uis.getValueExpression("value");
+				ELContext elContext = context.getELContext();
+
+				List<Type> types = (List<Type>) valueExpression.getValue(elContext);
+
+				for (Type type : types) {
+					if (type.getName().equals(value)) {
+						return type;
+					}
+				}
+			}
 		}
-		
+		return null;
+
 	}
 
 	@Override
