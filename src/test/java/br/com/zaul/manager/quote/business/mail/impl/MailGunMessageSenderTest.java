9541502c094c403af693bb204d928b27e1632150
thiago <dev.thiago@gmail.com>
2013-11-11 22:17:04 -0300

    321

--- src/test/java/br/com/zaul/manager/quote/business/mail/impl/MailGunMessageSenderTest.java
+++ src/test/java/br/com/zaul/manager/quote/business/mail/impl/MailGunMessageSenderTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import br.com.zaul.manager.quote.business.mail.contract.EmailSender;
-
 @RunWith(MockitoJUnitRunner.class)
 public class MailGunMessageSenderTest {
 	
@@ -13,9 +11,6 @@ public class MailGunMessageSenderTest {
 
 	@Test
 	public void testMail() {
-		EmailSender sender = new MailGunMessageSender();
-		
-		sender.from("thiago.fonseca@arkhi.com.br").to("dev.thiago@gmail.com").withTheSubject("Test").withTheText("Texto").send();
 		
 	}
 }
